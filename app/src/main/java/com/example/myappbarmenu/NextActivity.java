a91360f21271f2e6f8973be4d4b8372cafecf3a3
merkel1860 <merkelmller@yandex.ru>
2021-07-02 15:34:56 -0400

    Initial Commit 1. AppBar 2. Navigation between activities 3. Passing serializable object from one activity to another 4. Intent without result 5. Include Toolbar as external resource 6. Customized back button implemented as Icon 7. ActionBar with Icon which create a new memo ready to be edited 8. Save on both plateforms firebase and sqlLite or/and file 9. Inserting Back Button for each secondary activity

--- app/src/main/java/com/example/myappbarmenu/NextActivity.java
+++ app/src/main/java/com/example/myappbarmenu/NextActivity.java
@@ -1,17 +1,28 @@
 package com.example.myappbarmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.AppCompatEditText;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.MenuItem;
+//import android.widget.EditText;
+//import android.widget.EditText;
 
 public class NextActivity extends AppCompatActivity {
 
+    private LinearLayoutCompat parentContainer;
     AppCompatTextView userTextView;
+    AppCompatEditText editTextMultiline;
+
     Person receivedPerson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,9 @@ public class NextActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        wiringLocalWidgetsToScreenControls();
+
+        parentContainer = findViewById(R.id.next_container);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -27,15 +41,53 @@ public class NextActivity extends AppCompatActivity {
 
     }
 
+    private void wiringLocalWidgetsToScreenControls() {
+        editTextMultiline = new AppCompatEditText(getApplicationContext());
+        editTextMultiline.setInputType(InputType.TYPE_CLASS_TEXT |
+                InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_next_page,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_memo:
+                // TODO configure this function by letting it update UI with a textarea
+                // TODO wich should be used to write dowwn memos
+                setupEditMultiLineText();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void setupEditMultiLineText() {
+
+    }
+
     private void updateViewFromPassedArgs() {
         receivedPerson = (Person) getIntent().getSerializableExtra("user");
         userTextView = findViewById(R.id.received_user_text_view);
         userTextView.setText(receivedPerson.getFullname());
+    }
+    // Create a LayoutParams
+    private LinearLayoutCompat.LayoutParams makeLayoutParams(int weight, int rightMargin, int leftMargin,
+                                                             int topMarging, int bottomMarging) {
+        LinearLayoutCompat.
+                LayoutParams buttonLayoutParams = new LinearLayoutCompat.
+                LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+        buttonLayoutParams.weight = weight;
+        buttonLayoutParams.rightMargin = rightMargin;
+        buttonLayoutParams.leftMargin = leftMargin;
+        buttonLayoutParams.topMargin = topMarging;
+        buttonLayoutParams.bottomMargin = bottomMarging;
+        return buttonLayoutParams;
     }
 }
